3943e2315da14237cc55a7d4f41be02a23cc8a18
wangbaowei <wangbaowei@hba.cn>
2020-01-08 10:26:46 +0800

    flume specifies the number field format

---
+++ hba-log-analysis/src/main/java/cn.hba/syslog/service/impl/LogStrategyImpl.java
@@ -0,0 +1,17 @@
+package cn.hba.syslog.service.impl;
+
+import cn.hba.syslog.service.LogStrategy;
+import cn.hutool.json.JSONArray;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author wbw
+ * @date 2019/12/16 15:23
+ */
+@Service
+public class LogStrategyImpl implements LogStrategy {
+    @Override
+    public void stream(JSONArray val) {
+
+    }
+}
